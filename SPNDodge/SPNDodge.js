90eafb3d3180304c1524f3027b40b7a133b200a1
Hannah Dubiel <cartooncat101@gmail.com>
2016-10-29 16:40:03 -0400

    save

--- SPNDodge/SPNDodge.js
+++ SPNDodge/SPNDodge.js
@@ -4,8 +4,20 @@ class Boot {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
   }
-}
 
+  create(){
+    this.state.start("Generate")
+  }
+}
+  class Generate {
+    preload(){
+      console.log("Generating...");
+    }
+    create() {
+      console.log("All Done.");
+    }
+  }
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
+game.state.add("Generate",Generate);
 game.state.start("Boot");
