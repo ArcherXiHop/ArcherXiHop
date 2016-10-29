1604bf25b42e3f5e517ff03a7356f6e5c8e2a53d
Hannah Dubiel <cartooncat101@gmail.com>
2016-10-29 17:05:18 -0400

    save

--- SPNDodge/SPNDodge.js
+++ SPNDodge/SPNDodge.js
@@ -11,14 +11,21 @@ class Boot {
   class Generate {
     preload(){
       console.log("Generating...");
-      this.load.image("bg", "backgroundpotato.png")
+      this.load.image("bg", "backgroundpotato.png");
     }
     create() {
       console.log("All Done.");
+      this.state.start("Begin")
+    }
+  }
+  class Begin {
+    create() {
+      console.log("Entered Beginning Stage");
     }
   }
 
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
 game.state.add("Generate",Generate);
+game.state.add("Begin",Begin);
 game.state.start("Boot");
