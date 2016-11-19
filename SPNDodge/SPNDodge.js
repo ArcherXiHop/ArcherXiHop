3856700c6dfb912e57cec3f585395481a94fd041
Hannah Dubiel <cartooncat101@gmail.com>
2016-11-19 16:23:14 -0500

    save

--- SPNDodge/SPNDodge.js
+++ SPNDodge/SPNDodge.js
@@ -14,7 +14,9 @@ var C = {
     "file":"dodgesprite.png",
     "width": 60,
     "height": 75,
-    "frames": 4
+    "frames": 4,
+    "startx": 160,
+    "starty": 500
   }
 }
 class Boot {
@@ -43,6 +45,12 @@ class Boot {
       console.log("Entered Beginning Stage");
       this.background = this.add.tileSprite(0,0,C.bg.width,C.bg.height,"bg");
       this.background.autoScroll(C.bg.xspeed,C.bg.yspeed);
+      this.bg = this.add.tileSprite(0,0,C.bg.width,C.bg.height,"bg");
+      this.bg.autoScroll(C.bg.xspeed,C.bg.yspeed);
+      this.dodger = this.add.sprite(C.p.startx,C.p.starty,"dodger");
+      this.dodger.anchor.set(0.5,0.5);
+      this.dodger.smoothed = false;
+      this.dodger.scale.set(1);
     }
   }
   function restart() {
